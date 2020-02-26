ca22bc20731e2c2e91e7776e7b57c3c69044a549
evgrussia <50201814+evgrussia@users.noreply.github.com>
2020-02-26 20:35:41 +0300

    Add files via upload

---
+++ HeadHunterAPI/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace HeadHunterAPI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DataJson = new System.Windows.Forms.DataGridView();
+            this.SearchBox = new System.Windows.Forms.TextBox();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.ColId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColSalaryFrom = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColSalTo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColEmpl = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColAddress = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColInfo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.DataJson)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DataJson
+            // 
+            this.DataJson.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataJson.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColId,
+            this.ColName,
+            this.ColSalaryFrom,
+            this.ColSalTo,
+            this.ColEmpl,
+            this.ColAddress,
+            this.ColInfo});
+            this.DataJson.Location = new System.Drawing.Point(12, 2);
+            this.DataJson.Name = "DataJson";
+            this.DataJson.Size = new System.Drawing.Size(785, 273);
+            this.DataJson.TabIndex = 3;
+            // 
+            // SearchBox
+            // 
+            this.SearchBox.Location = new System.Drawing.Point(12, 290);
+            this.SearchBox.Name = "SearchBox";
+            this.SearchBox.Size = new System.Drawing.Size(289, 20);
+            this.SearchBox.TabIndex = 4;
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(320, 290);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(75, 23);
+            this.SearchButton.TabIndex = 5;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // ColId
+            // 
+            this.ColId.HeaderText = "ID";
+            this.ColId.Name = "ColId";
+            // 
+            // ColName
+            // 
+            this.ColName.HeaderText = "Name";
+            this.ColName.Name = "ColName";
+            // 
+            // ColSalaryFrom
+            // 
+            this.ColSalaryFrom.HeaderText = "Salary From";
+            this.ColSalaryFrom.Name = "ColSalaryFrom";
+            // 
+            // ColSalTo
+            // 
+            this.ColSalTo.HeaderText = "Salary To";
+            this.ColSalTo.Name = "ColSalTo";
+            // 
+            // ColEmpl
+            // 
+            this.ColEmpl.HeaderText = "Employer";
+            this.ColEmpl.Name = "ColEmpl";
+            // 
+            // ColAddress
+            // 
+            this.ColAddress.HeaderText = "Address";
+            this.ColAddress.Name = "ColAddress";
+            // 
+            // ColInfo
+            // 
+            this.ColInfo.HeaderText = "Info";
+            this.ColInfo.Name = "ColInfo";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(419, 293);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "--";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 334);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.SearchBox);
+            this.Controls.Add(this.DataJson);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DataJson)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DataJson;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColSalaryFrom;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColSalTo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColEmpl;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColAddress;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColInfo;
+        private System.Windows.Forms.TextBox SearchBox;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ HeadHunterAPI/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HHLibrary;
+
+namespace HeadHunterAPI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        HHModel model = new HHModel();
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            
+            List<Vacancy> vacancies = model.JsonParceString(model.RequestGet("https://api.hh.ru/vacancies?text="+SearchBox.Text));
+            DataJson.Rows.Clear();
+            label1.Text = vacancies.Count()+" вакансий всего";
+            foreach (Vacancy v in vacancies)
+            {
+                DataGridViewRow row = (DataGridViewRow)DataJson.Rows[0].Clone();
+                row.Cells[0].Value = v.Area;
+                row.Cells[1].Value = v.Name;
+                row.Cells[2].Value = v.SalaryFrom;
+                row.Cells[3].Value = v.SalaryTo;
+                row.Cells[4].Value = v.EmployerName;
+                row.Cells[5].Value = v.AddressStreet;
+                row.Cells[6].Value = v.Info;
+                DataJson.Rows.Add(row);
+            }
+        }
+    }
+}
